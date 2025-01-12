bb38c127d6f64d7b3ecfd0e00e279850fd7e9cd3
Samuda Paranayapa <aparanayapa@gmail.com>
2025-01-13 01:26:56 +0530

    Separate product data and add cart functionality
    Moved product objects from home.js to a new products.js file for better modularity and organization.
    Created an add to cart button with interactive functionality to handle user actions.
    Developed a new cart.js file and implemented an array to store data from the add to cart button actions.
    Enhanced code structure and maintainability by separating concerns across files.

---
+++ resource/scripts/data/product.js
@@ -0,0 +1,56 @@
+const product = [{
+  id : '001',
+  image : 'resource/product/iphone-13-pro.webp',
+  name :'Apple iphone 13 pro - 256GB',
+  rating :{
+    stars:'resource/ratings/rating-45.png',
+    count: 28
+  },
+  priceCent:17000000,
+  },{
+  id : '002',
+  image : 'resource/product/iso-100-protein-powder.jpg',
+  name :'Dymatize ISO 100 - 5lb',
+  rating :{
+    stars:'resource/ratings/rating-40.png',
+    count: 127
+  },
+  priceCent:3400000,
+  },{
+  id : '003',
+  image : 'resource/product/black-gray-sport-tshirt.webp',
+  name :'Black & Gray Sport T-shirt',
+  rating :{
+    stars:'resource/ratings/rating-50.png',
+    count: 12
+  },
+  priceCent:130000,
+  },{
+    id : '004',
+    image : 'resource/product/ladies-handbag.png',
+    name :'Black Ladies Handbag & purse',
+    rating :{
+      stars:'resource/ratings/rating-25.png',
+      count: 41
+    },
+    priceCent:470000,
+    },{
+      id : '005',
+      image : 'resource/product/Xiaomi-10000mAh-powerbank.webp',
+      name:'Xiaomi Mi Powerbank - 10000mAh',
+      rating :{
+        stars:'resource/ratings/rating-30.png',
+        count: 84
+      },
+      priceCent:1800000,
+      },{
+      id : '006',
+      image : 'resource/product/cerave-hydrating-cleanser.jpg',
+      name :'Cerave hydrating cleanser',
+      rating :{
+        stars:'resource/ratings/rating-40.png',
+        count: 102
+      },
+      priceCent:220000,
+      }
+];
--- resource/scripts/home.js
+++ resource/scripts/home.js
@@ -1,54 +1,3 @@
-const product = [{
-  image : 'resource/product/iphone-13-pro.webp',
-  name :'Apple iphone 13 pro - 256GB',
-  rating :{
-    stars:'resource/ratings/rating-45.png',
-    count: 28
-  },
-  priceCent:17000000,
-  },{
-  image : 'resource/product/iso-100-protein-powder.jpg',
-  name :'Dymatize ISO 100 - 5lb',
-  rating :{
-    stars:'resource/ratings/rating-40.png',
-    count: 127
-  },
-  priceCent:3400000,
-  },{
-  image : 'resource/product/black-gray-sport-tshirt.webp',
-  name :'Black & Gray Sport T-shirt',
-  rating :{
-    stars:'resource/ratings/rating-50.png',
-    count: 12
-  },
-  priceCent:130000,
-  },{
-    image : 'resource/product/ladies-handbag.png',
-    name :'Black Ladies Handbag & purse',
-    rating :{
-      stars:'resource/ratings/rating-25.png',
-      count: 41
-    },
-    priceCent:470000,
-    },{
-      image : 'resource/product/Xiaomi-10000mAh-powerbank.webp',
-      name:'Xiaomi Mi Powerbank - 10000mAh',
-      rating :{
-        stars:'resource/ratings/rating-30.png',
-        count: 84
-      },
-      priceCent:1800000,
-      },{
-      image : 'resource/product/cerave-hydrating-cleanser.jpg',
-      name :'Cerave hydrating cleanser',
-      rating :{
-        stars:'resource/ratings/rating-40.png',
-        count: 102
-      },
-      priceCent:220000,
-      }
-];
-
 let productHTML = '';
 
 /*Generating HTML code for each objects in product class*/ 
@@ -94,7 +43,7 @@ product.forEach((product) => {
         Added
       </div>
 
-      <button class="add-to-cart-button button-primary">
+      <button class="add-to-cart-button button-primary js-add-to-cart-button" data-product-id = "${product.id}">
         Add to Cart
       </button>
     </div>
@@ -102,3 +51,30 @@ product.forEach((product) => {
 });
 
 document.querySelector('.js-product-grid').innerHTML = productHTML;
+
+document.querySelectorAll('.js-add-to-cart-button')
+
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+  
+      let matchingitem;
+
+      cart.forEach((item) => {
+        if(productId === item.productId){
+          matchingitem = item;
+        }
+      });
+
+      if (matchingitem){
+        matchingitem.quantity += 1;
+      }else{
+        cart.push({
+        productId : productId,
+        quantity : 1
+        });
+      }
+      console.log(cart);
+    });
+  });
+  
