601d5ca32a4e6f7e3b721e98447ea1369e488b71
Samuda Paranayapa <aparanayapa@gmail.com>
2025-01-15 01:35:07 +0530

    The delete link has been made interactive by adding functionality to remove items from the cart. When the delete link is clicked, it triggers the removal of the corresponding item from the DOM and ensures the cart is updated accordingly. This improves the user experience by allowing easier cart management.

--- resource/scripts/checkout.js
+++ resource/scripts/checkout.js
@@ -18,7 +18,7 @@ cart.forEach((cartItem) => {
  
 
   cartHTML += `
-            <div class="cart-item-container">
+            <div class="cart-item-container js-cart-item-container-${productId}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -102,8 +102,11 @@ document.querySelectorAll('.js-delete-link').forEach((link) => {
   link.addEventListener('click', () => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
-    console.log(cart);
+    
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.remove();
+
   });
 });
-
-console.log(cart);
